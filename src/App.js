d562b543ca19df74c35b79480fa9ddeb6b4f8dd8
Wario <37343942+IfreannMedia@users.noreply.github.com>
2021-01-13 19:32:11 +0100

    added nav and background gradient
    imported tachyons for styling and tilt for some visual effect, used the background generator to get a nice gradient background
    add eslintcache to the gitignore

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from "./components/navigation.js";
+import Logo from "./components/logo/logo.js";
 
 function App() {
   return (
     <div className="App">
-
       <Navigation/>
-      {/*<Logo />
-      <ImageLinkForm />
+      <Logo />
+      {/*<ImageLinkForm />
       <FaceRecognition />*/}
     </div>
   );
